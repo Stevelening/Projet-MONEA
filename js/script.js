04c0b30e572f3610861e0fcc1b388f41aa746bf2
Stevelening <micheltalaupa@gmail.com>
2022-05-29 00:03:37 +0100

    Remise de la totalité du travail

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+  $(document).ready(function(){
+    $('.Carousel2').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+    });
+  });
